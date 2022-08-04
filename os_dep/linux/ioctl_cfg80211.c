9011727b418ae0eac3890f724bd2d62104844a43
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-08-04 22:44:48 +0200

    Fix references to wdev->ssid and wdev->ssid_len
    This fix follows the adaptions made in
    7b0a0e3c3a88260b6fcb017e49f198463aa62ed1 of the Linux Kernel.

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -879,8 +879,13 @@ static int _cfg80211_check_bss(struct _ADAPTER *a)
 	wdev = a->rtw_wdev;
 	network = &a->mlmeextpriv.mlmext_info.network;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2)
+	if ((!wdev->u.ap.ssid_len) || (wdev->u.ap.ssid_len != network->Ssid.SsidLength)
+	    || (_rtw_memcmp(wdev->u.ap.ssid, network->Ssid.Ssid,
+#else
 	if ((!wdev->ssid_len) || (wdev->ssid_len != network->Ssid.SsidLength)
 	    || (_rtw_memcmp(wdev->ssid, network->Ssid.Ssid,
+#endif
 			    network->Ssid.SsidLength) == _FALSE)) {
 		RTW_PRINT(FUNC_ADPT_FMT ": bssid:"MAC_FMT"\n",
 			  FUNC_ADPT_ARG(a), MAC_ARG(network->MacAddress));
@@ -888,7 +893,11 @@ static int _cfg80211_check_bss(struct _ADAPTER *a)
 			  FUNC_ADPT_ARG(a), network->Ssid.Ssid,
 			  network->Ssid.SsidLength);
 		RTW_PRINT(FUNC_ADPT_FMT ": (wdev) ssid:[%s] len=%d\n",
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2)
+			  FUNC_ADPT_ARG(a), wdev->u.ap.ssid, wdev->u.ap.ssid_len);
+#else
 			  FUNC_ADPT_ARG(a), wdev->ssid, wdev->ssid_len);
+#endif
 
 		return _FALSE;
 	}
